c5c7104a8bf4a048aa61f813893a8ac33d1345c8
Kikker2022 <andries1963@gmail.com>
2026-03-25 21:01:03 +0100

    Add dice roll function and display random question

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+import alleVragen from "./data/vragen.js";
 
+function gooiDobbelsteen(){
+
+let worp = Math.floor(Math.random()*6)+1;
+
+document.getElementById("dobbel").innerText = "Je gooide: " + worp;
+
+let vraag = alleVragen[Math.floor(Math.random()*alleVragen.length)];
+
+document.getElementById("vraag").innerText = vraag.vraag;
+
+}
+
+window.gooiDobbelsteen = gooiDobbelsteen;
