fecd2674aa95f5244712725fc1764388fd6eac1a
Kikker2022 <andries1963@gmail.com>
2026-03-27 20:08:43 +0100

    Refactor gooiDobbelsteen function
    Refactor gooiDobbelsteen function to include random dice roll logic.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import alleVragen from "./data/vragen.js";
 
-function gooiDobbelsteen(){
-
+function gooiDobbelsteen() {
+  const dobbel = Math.floor(Math.random() * 6) + 1;
+  document.getElementById("dobbel").innerText = dobbel;
+}
 let worp = Math.floor(Math.random()*6)+1;
 
 document.getElementById("dobbel").innerText = "Je gooide: " + worp;
