02cf18782a7ed5d66b978d2bf3add2701a396fdb
Kikker2022 <andries1963@gmail.com>
2026-03-31 18:26:21 +0200

    Refactor alleVragen structure for consistency

--- data/vragen.js
+++ data/vragen.js
@@ -1,16 +1,43 @@
-
 export const alleVragen = [
-  {vraag:"Wat is de hoofdstad van Nederland?", antwoord:"Amsterdam", rubriek:"TopoVMBO", moeilijkheid:1},
-  {vraag:"Welke provincie ligt helemaal in het noorden?", antwoord:"Groningen", rubriek:"TopoVMBO", moeilijkheid:1},
-  {vraag:"Welke zee ligt ten noorden van Nederland?", antwoord:"Noordzee", rubriek:"TopoVMBO", moeilijkheid:2},
 
-  {vraag:"Wat is de hoofdplaats van Ooststellingwerf?", antwoord:"Oosterwolde", rubriek:"Oost15", moeilijkheid:1},
-  {vraag:"In welke provincie ligt Ooststellingwerf?", antwoord:"Friesland", rubriek:"Oost15", moeilijkheid:1},
+{
+vraag: "Wat is de hoofdstad van Nederland?",
+antwoord: "Amsterdam"
+},
 
-  {vraag:"Welke plaats ligt bij het Drents-Friese Wold?", antwoord:"Appelscha", rubriek:"Oost40", moeilijkheid:2},
+{
+vraag: "Welke provincie ligt in het noorden?",
+antwoord: "Groningen"
+},
 
-  {vraag:"Wat meet een tachymeter?", antwoord:"Afstanden en hoeken", rubriek:"Landmeetkunde", moeilijkheid:3},
-  {vraag:"Waar staat RTK voor?", antwoord:"Realtime kinematic", rubriek:"Landmeetkunde", moeilijkheid:3}
+{
+vraag: "Welke zee ligt ten noorden van Nederland?",
+antwoord: "Noordzee"
+},
+
+{
+vraag: "Wat is de hoofdplaats van Ooststellingwerf?",
+antwoord: "Oosterwolde"
+},
+
+{
+vraag: "In welke provincie ligt Ooststellingwerf?",
+antwoord: "Friesland"
+},
+
+{
+vraag: "Welke plaats ligt bij het Drents-Friese Wold?",
+antwoord: "Appelscha"
+},
+
+{
+vraag: "Wat meet een tachymeter?",
+antwoord: "Afstanden en hoeken"
+},
+
+{
+vraag: "Waar staat RTK voor?",
+antwoord: "Realtime Kinematic"
+}
+
 ];
-
-
