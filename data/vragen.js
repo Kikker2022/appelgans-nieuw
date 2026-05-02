80c3b620777de46e298223ed92f94352830cb995
Kikker2022 <andries1963@gmail.com>
2026-05-02 21:40:16 +0200

    Update vragen.js with new questions and answers

--- data/vragen.js
+++ data/vragen.js
@@ -1,28 +1,42 @@
 export default [
-
-{
-vraag: "Wat is de hoofdstad van Nederland?",
-antwoord: "Amsterdam"
-},
-
-{
-vraag: "Hoeveel dagen heeft een week?",
-antwoord: "7"
-},
-
-{
-vraag: "Wat is 2 + 2?",
-antwoord: "4"
-},
-
-{
-vraag: "Welke kleur krijg je van blauw en geel?",
-antwoord: "Groen"
-},
-
-{
-vraag: "Hoeveel maanden heeft een jaar?",
-antwoord: "12"
-}
-
+  {
+    vraag: "Wat is de hoofdstad van Nederland?",
+    antwoord: "Amsterdam"
+  },
+  {
+    vraag: "Hoeveel dagen heeft een week?",
+    antwoord: "7"
+  },
+  {
+    vraag: "Wat is 5 x 6?",
+    antwoord: "30"
+  },
+  {
+    vraag: "Welke kleur krijg je door blauw en geel te mengen?",
+    antwoord: "Groen"
+  },
+  {
+    vraag: "Wat is de grootste planeet?",
+    antwoord: "Jupiter"
+  },
+  {
+    vraag: "Wie schreef Harry Potter?",
+    antwoord: "J.K. Rowling"
+  },
+  {
+    vraag: "Hoeveel minuten zitten er in een uur?",
+    antwoord: "60"
+  },
+  {
+    vraag: "Wat is de hoofdstad van Duitsland?",
+    antwoord: "Berlijn"
+  },
+  {
+    vraag: "Hoeveel continenten zijn er?",
+    antwoord: "7"
+  },
+  {
+    vraag: "Wat is 12 + 8?",
+    antwoord: "20"
+  }
 ];
