61294c43d86f0dc224f8248eb57ad5ee056dd052
Kikker2022 <andries1963@gmail.com>
2026-05-05 22:10:55 +0200

    Update questions and answers in vragen.js
    Replaced existing questions and answers with new ones.

--- data/vragen.js
+++ data/vragen.js
@@ -1,42 +1,5 @@
-export default [
-  {
-    vraag: "Wat is de hoofdstad van Nederland?",
-    antwoord: "Amsterdam"
-  },
-  {
-    vraag: "Hoeveel dagen heeft een week?",
-    antwoord: "7"
-  },
-  {
-    vraag: "Wat is 5 x 6?",
-    antwoord: "30"
-  },
-  {
-    vraag: "Welke kleur krijg je door blauw en geel te mengen?",
-    antwoord: "Groen"
-  },
-  {
-    vraag: "Wat is de grootste planeet?",
-    antwoord: "Jupiter"
-  },
-  {
-    vraag: "Wie schreef Harry Potter?",
-    antwoord: "J.K. Rowling"
-  },
-  {
-    vraag: "Hoeveel minuten zitten er in een uur?",
-    antwoord: "60"
-  },
-  {
-    vraag: "Wat is de hoofdstad van Duitsland?",
-    antwoord: "Berlijn"
-  },
-  {
-    vraag: "Hoeveel continenten zijn er?",
-    antwoord: "7"
-  },
-  {
-    vraag: "Wat is 12 + 8?",
-    antwoord: "20"
-  }
+const vragen = [
+  { vraag: "Wat is de hoofdstad van Frankrijk?", antwoord: "Parijs" },
+  { vraag: "Hoeveel dagen heeft een jaar?", antwoord: "365" },
+  { vraag: "Wat is 2 + 2?", antwoord: "4" }
 ];
